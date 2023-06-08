a6fd0be5ab57185acf831949cb7b5ddc56f528e8
GuruK <kakky-github@m.qubeley.com>
2023-06-08 19:05:50 +0900

    fixed code for latest iced

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,105 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{
+    alignment, executor,
+};
+use iced::{
+    Alignment, Application, Command, Element, Length, Settings, Subscription,
+    Theme,
+};
+use iced::widget::{
+    Container, Text, Column, Image, 
+};
+use std::path::PathBuf;
+
+pub fn main() -> iced::Result {
+    // フォントを指定しつつ実行する。
+    Events::run(Settings {
+        default_font: Some(include_bytes!("../fonts/NotoSansJP-Regular.ttf")),
+        ..Settings::default()
+    })
+}
+
+// メインとなる構造体。アプリで保持する状態を変数にする。
+#[derive(Debug, Default)]
+struct Events {
+    path: PathBuf,
+}
+
+// 何らかの変更があったときに飛ぶメッセージ。今回はイベント発生のみ。
+#[derive(Debug, Clone)]
+enum Message {
+    EventOccurred(iced::event::Event),
+}
+
+impl Application for Events {
+    type Executor = executor::Default;
+    type Message = Message;
+    type Flags = ();
+    type Theme = Theme;
+
+    fn new(_flags: ()) -> (Events, Command<Message>) {
+        (Events::default(), Command::none())
+    }
+
+    // ウィンドウのタイトル。状態に合わせた動的な生成も可。
+    fn title(&self) -> String {
+        String::from("Image Viewer")
+    }
+
+    // 何らかの変更があったときに呼び出される。
+    // 発生した事柄はenum（今回の場合はMessage）として伝えられる。
+    // Icedのバージョン0.3から引数にClipboardが増えたが、使わないので無視。
+    fn update(&mut self, message: Message) -> Command<Message> {
+        // ファイルがドロップされたときに、アプリの状態を変更する。
+        // Eventのenumの中に、イベントの内容（別のEventのenum）とか、
+        // 今回のFileDroppedではファイルパスが含まれたりする。
+        match message {
+            Message::EventOccurred(event) => {
+                if let iced::event::Event::Window(we) = event {
+                    if let iced::window::Event::FileDropped(path) = we {
+                        self.path = path;
+                    }
+                }
+            }
+        };
+
+        Command::none()
+    }
+
+    // イベントが発生したときに呼び出される。マウス操作、ウィンドウ関係、キーボード操作等。
+    // 何らかのSubscriptionを返すことで、update()が実行される。
+    fn subscription(&self) -> Subscription<Message> {
+        iced::subscription::events().map(Message::EventOccurred)
+    }
+
+    // 表示されるGUIを生成する。
+    fn view(&self) -> Element<Message> {
+        // ファイルパス表示部
+        let mut p = self.path.to_str().unwrap_or("").to_string();
+        if p.is_empty() {
+            p = String::from("画像ファイルをウィンドウにドロップしてね。");
+        }
+        let path = Container::new(Text::new(p).size(20)).padding(4);
+
+        // 画像表示部
+        let image = Container::new(
+            Image::new(self.path.clone())
+                .width(Length::Fill)
+                .height(Length::Fill),
+        )
+        .height(Length::Fill)
+        .width(Length::Fill)
+        .align_x(alignment::Horizontal::Center)
+        .align_y(alignment::Vertical::Center);
+        
+        let content = Column::new()
+            .width(Length::Fill)
+            .align_items(Alignment::Start)
+            .push(path)
+            .push(image);
+
+        Container::new(content)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .into()
+        }
 }
